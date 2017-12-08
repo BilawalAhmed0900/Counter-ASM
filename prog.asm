14d1a796ce8c9418c26a3968b09b350e12e48f10
Mian Bilawal <blwal7057@gmail.com>
2017-12-08 09:39:03 +0500

    Formatted and used EBP

--- prog.asm
+++ prog.asm
@@ -10,31 +10,33 @@ includelib c:\masm32\lib\kernel32.lib
 includelib c:\masm32\lib\masm32.lib 
 
 .data
-    _ret sdword 13  ;mov to next line
-    _car sdword 10  ;return to start of the line
-    
+    _ret sdword 13                          ;mov to next line
+    _car sdword 10                          ;return to start of the line    
    
 .code
 main:
-    mov edx, 'A'    ;'A'
-    mov ecx, 58     ;how many times
+    mov edx, 'A'                            ;'A'
+    mov ecx, 58                             ;how many times
     jmp _end
     
 _loop:  
-    push ecx        ;save ecx, because invoke StdOut make changes to it
-    push edx        ;save edx; same reason
-    invoke StdOut, esp ;esp store latest stack pointer
+    push ecx                                ;save ecx, because invoke StdOut make changes to it
+    push edx                                ;save edx; same reason
+    mov ebp, esp                            ;saving that address to esp from esp which stores pointer to top of the stack,
+                                            ;in current case address of edx stored value
+                                            
+    invoke StdOut, ebp                      ;ebp stored the latest pointer
     invoke StdOut, addr _ret
     invoke StdOut, addr _car
-    pop edx         ;pop back edx
-    pop ecx         ;pop back ecx
+    pop edx                                 ;pop back edx
+    pop ecx                                 ;pop back ecx
     
-    inc edx         ;increase edx, e.g. from 'A' to 'B'
-    dec ecx         ;decrease counter
+    inc edx                                 ;increase edx, e.g. from 'A' to 'B'
+    dec ecx                                 ;decrease counter
      
 _end:
-    cmp ecx, 0      ;check if ecx is zero i.e. loop ended
-    jne _loop       ;if ecx not zero, just to start of loop
+    cmp ecx, 0                              ;check if ecx is zero i.e. loop ended
+    jne _loop                               ;if ecx not zero, just to start of loop
 
 _exit:    
     invoke ExitProcess, 0
